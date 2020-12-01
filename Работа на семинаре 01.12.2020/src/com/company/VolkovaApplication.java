870fe3eaefad8f30bbc112dad230441ec4d9bbb6
Volkova Kristina <kristina.volkova18@yandex.ru>
2020-12-01 20:12:46 +0300

    Create VolkovaApplication.java

---
+++ Работа на семинаре 01.12.2020/src/com/company/VolkovaApplication.java
@@ -0,0 +1,13 @@
+package com.example.volkova;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VolkovaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VolkovaApplication.class, args);
+    }
+
+}
